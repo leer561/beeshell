8a66acc54a720f6cf502dac036be362ee385debb
leer <leer561@gmail.com>
2020-01-10 20:27:49 +0800

    datepicker无药可救 恢复原样

--- dist/components/Datepicker/index.js
+++ dist/components/Datepicker/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import {range, isLeapYear, convert2Digit} from '../../common/utils';
-import {noop} from '../../common/utils/fns';
-import {Scrollpicker} from '../Scrollpicker';
-
+import { range, isLeapYear, convert2Digit } from '../../common/utils';
+import { noop } from '../../common/utils/fns';
+import { Scrollpicker } from '../Scrollpicker';
 export class Datepicker extends React.Component {
 	constructor(props) {
 		super(props);
 		this.handleChange = (index1, index2) => {
-			const {list, value} = this.state;
+			const { list, value } = this.state;
 			let newList = [...list];
 			let newValue = [...value];
 			newValue[index1] = index2;
@@ -27,7 +26,7 @@ export class Datepicker extends React.Component {
 				year = list[0][value[0]];
 				month = list[1][value[1]];
 			}
-			days = this.days ? this.days : this.getDays(year, month);
+			days = this.getDays(year, month);
 			newList[2] = days;
 			day = list[2][value[2]];
 			if (days.indexOf(day) === -1) {
@@ -45,63 +44,26 @@ export class Datepicker extends React.Component {
 			...data
 		};
 	}
-
 	initialize(props) {
-		let {startYear, numberOfYears, date, endDate, showPastDate} = props;
-
-		// 当前年月日
-		const d = new Date()
-		const currentYear = d.getFullYear()
-		const currentMonth = d.getMonth() + 1
-		const currentDay = d.getUTCDate()
-
-		let years = range(numberOfYears).map((item, index) => {
+		let { startYear, numberOfYears, date } = props;
+		const years = range(numberOfYears).map((item, index) => {
 			return Number(startYear) + index;
 		});
-		let months = range(12).map(i => {
+		const months = range(12).map(i => {
 			return i + 1;
 		});
 		const input = this.getDateInputByString(date);
 		if (!input) {
 			date = new Date();
-		} else {
+		}
+		else {
 			date = new Date(input[0], input[1], input[2]);
 		}
-
 		const year = date.getFullYear();
-		let month = date.getMonth() + 1;
+		const month = date.getMonth() + 1;
 		const day = date.getDate();
-		let days = this.getDays(year, month);
-		this.days = days
-		// 如果是当前年份过滤掉之前月份
-		if (year == currentYear && !showPastDate) {
-			months = months.filter(month => month >= currentMonth)
-			if (month == currentMonth) {
-				days = days.filter(day => day >= currentDay)
-				this.days = days
-			}
-		}
-
-		// endDate调整截止时间
-		if (endDate !== undefined) {
-			const endDateArray = endDate.split('-')
-			const [endYear, endMonth, endDay] = endDateArray
-			years = years.filter(year => year <= endYear)
-			// 如果未跨年
-			if (input && input[0] == endYear) {
-				months = months.filter(month => month <= endMonth)
-				// 并且 当前年份不是endYear 则是跨年
-				if(currentYear!=endYear){
-					month = months[0]
-				}
-			}
-			// 判断当前天数
-			if (input && (input[1] + 1) == endMonth) {
-				days = days.filter(day => day <= endDay)
-				this.days = days
-			}
-		}
-		const list = [years, months, days]
+		const days = this.getDays(year, month);
+		const list = [years, months, days];
 		const value = [
 			years.indexOf(year),
 			months.indexOf(month),
@@ -112,7 +74,6 @@ export class Datepicker extends React.Component {
 			value
 		};
 	}
-
 	getDateInputByString(param) {
 		if (!param) {
 			return null;
@@ -135,7 +96,6 @@ export class Datepicker extends React.Component {
 		}
 		return input;
 	}
-
 	getDays(year, month) {
 		year = parseInt(year, 10);
 		month = parseInt(month, 10);
@@ -165,15 +125,13 @@ export class Datepicker extends React.Component {
 		});
 		return days;
 	}
-
 	componentWillReceiveProps(nextProps) {
-		if (nextProps.date !== this.props.date) {
+		if (nextProps.date !== this.props) {
 			this.setState({
 				...this.initialize(nextProps)
 			});
 		}
 	}
-
 	getViewList(list) {
 		return list.map((item, index) => {
 			let tmp = [];
@@ -195,28 +153,21 @@ export class Datepicker extends React.Component {
 			return tmp;
 		});
 	}
-
 	getDateByIndex(list, value) {
 		const ret = list.map((item, index) => {
 			return convert2Digit(item[value[index]]);
 		});
 		return ret.join('-');
 	}
-
 	render() {
-		const {proportion} = this.props;
-		const {list, value} = this.state;
+		const { proportion } = this.props;
+		const { list, value } = this.state;
 		const viewList = this.getViewList(list);
-		return (React.createElement(Scrollpicker, Object.assign({}, this.props, {
-			list: viewList,
-			value: value,
-			onChange: this.handleChange
-		})));
+		return (React.createElement(Scrollpicker, Object.assign({}, this.props, { list: viewList, value: value, onChange: this.handleChange })));
 	}
 }
-
 Datepicker.defaultProps = {
-	startYear: (new Date()).getFullYear(),
+	startYear: 2018,
 	numberOfYears: 10,
 	date: 'undefined',
 	onChange: noop,
